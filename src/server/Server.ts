301006d3c2ed942b4e08e718adaa8ba6ea8f6913
Gabriel De Liz <gabriellizcfnpbnu@gmail.com>
2025-02-23 14:29:04 -0300

    feat: - nodemon config - insomnia - metods http

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -3,7 +3,7 @@ import express from "express";
 const server = express();
 
 server.get("/", (req, res) => {
-    res.send("Ola pessoal, tudo bem com vcs??");
+    res.send("Hello World!");
 });
 
 export { server };
